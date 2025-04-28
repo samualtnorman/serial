c355adbc06ceb3af2a5bf98eb41ab11e1a85e699
Samual Norman <me@samual.uk>
2025-04-28 13:38:55 +0100

    make encoder function expect type stored in `Schema`

--- src/encoder.ts
+++ src/encoder.ts
@@ -1,11 +1,11 @@
 import { ensure } from "@samual/lib/assert"
 import type { EncoderPlugin, Schema } from "./common"
 
-export const makeEncoder = (schema: Schema, plugins: EncoderPlugin[]) => {
+export const makeEncoder = <T>(schema: Schema<T>, plugins: EncoderPlugin[]) => {
 	const tagToPluginMap = new Map(plugins.map(plugin => [ plugin.tag, plugin ]))
 	
 	const callPlugin = (schema: Schema, value: unknown) =>
 		ensure(tagToPluginMap.get(schema.tag), HERE).encode(schema, callPlugin, value)
 
-	return (value: unknown) => ensure(callPlugin(schema, value), HERE)
+	return (value: T) => ensure(callPlugin(schema, value), HERE)
 }
