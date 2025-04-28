7acf56d6ce62e64279b51704261704850eab93d5
Samual Norman <me@samual.uk>
2025-04-28 14:21:17 +0100

    add float64 support

---
+++ src/float64.ts
@@ -0,0 +1,19 @@
+import { DecoderPlugin, EncoderPlugin, Schema } from "./common"
+
+const Float64Tag = Symbol()
+
+export const Float64Schema = { tag: Float64Tag } as Schema<number>
+
+export const Float64EncoderPlugin: EncoderPlugin = {
+	tag: Float64Tag,
+	encode(_schema, _callPlugin, value) {
+		if (typeof value == `number`)
+			return [ ...new Uint8Array(new Float64Array([ value ]).buffer) ]
+	}
+}
+
+export const Float64DecoderPlugin: DecoderPlugin = {
+	tag: Float64Tag,
+	decode: (_schema, _callPlugin, data, index = { $: 0 }) =>
+		new Float64Array(new Uint8Array(data.slice(index.$, index.$ += 8)).buffer)[0]
+}
