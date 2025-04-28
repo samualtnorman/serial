cf4dee208f51ac859ee53de17079c959a338b1cb
Samual Norman <me@samual.uk>
2025-04-28 11:06:28 +0100

    drop need for `AutoWeakMap` and just use `Map` that's initialised from `plugins` straight away instead

--- src/decoder.ts
+++ src/decoder.ts
@@ -1,12 +1,11 @@
 import { ensure } from "@samual/lib/assert"
-import { AutoWeakMap } from "@samual/lib/AutoMap"
 import type { DecoderPlugin, Schema } from "./common"
 
 export const makeDecoder = (schema: Schema, plugins: DecoderPlugin[]) => {
-	const schemaToPluginMap = new AutoWeakMap((schema: Schema) => plugins.find(plugin => plugin.tag == schema.tag))
+	const tagToPluginMap = new Map(plugins.map(plugin => [ plugin.tag, plugin ]))
 
 	const callPlugin = (schema: Schema, data: number[], index?: { $: number }) =>
-		ensure(schemaToPluginMap.get(schema)).decode(schema, callPlugin, data, index)
+		ensure(tagToPluginMap.get(schema.tag)).decode(schema, callPlugin, data, index)
 
 	return (data: number[], index?: { $: number }) => callPlugin(schema, data, index)
 }
--- src/encoder.ts
+++ src/encoder.ts
@@ -1,10 +1,11 @@
 import { ensure } from "@samual/lib/assert"
-import { AutoWeakMap } from "@samual/lib/AutoMap"
 import type { EncoderPlugin, Schema } from "./common"
 
 export const makeEncoder = (schema: Schema, plugins: EncoderPlugin[]) => {
-	const schemaToPluginMap = new AutoWeakMap((schema: Schema) => plugins.find(plugin => plugin.tag == schema.tag))
-	const callPlugin = (schema: Schema, value: unknown) => ensure(schemaToPluginMap.get(schema), HERE).encode(schema, callPlugin, value)
+	const tagToPluginMap = new Map(plugins.map(plugin => [ plugin.tag, plugin ]))
+	
+	const callPlugin = (schema: Schema, value: unknown) =>
+		ensure(tagToPluginMap.get(schema.tag), HERE).encode(schema, callPlugin, value)
 
 	return (value: unknown) => ensure(callPlugin(schema, value), HERE)
 }
