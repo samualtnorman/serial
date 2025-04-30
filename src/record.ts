4f8036b310debd15ba130de3092f71235acf6b02
Samual Norman <me@samual.uk>
2025-04-30 10:25:01 +0100

    add ability to encode and decode `Record<>`s

---
+++ src/record.ts
@@ -0,0 +1,43 @@
+import { assert } from "@samual/lib/assert"
+import { ArrayDecoderPlugin, ArrayEncoderPlugin, arraySchema } from "./array"
+import type { DecoderPlugin, EncoderPlugin, Schema } from "./common"
+import { ObjectDecoderPlugin, ObjectEncoderPlugin, objectSchema } from "./object"
+import { isRecord } from "@samual/lib/isRecord"
+import { getKeys } from "./internal"
+
+const RecordTag = Symbol(`Record`)
+type RecordTag = typeof RecordTag
+
+export type RecordSchema<T = unknown> = Schema<T> & { tag: RecordTag, schema: Schema<{ key: PropertyKey, value: unknown }[]> }
+
+export const recordSchema = <TKey extends PropertyKey, TValue>(keySchema: Schema<TKey>, valueSchema: Schema<TValue>) =>
+	({ tag: RecordTag, schema: arraySchema(objectSchema({ key: keySchema, value: valueSchema })) }) as any as RecordSchema<Record<TKey, TValue>>
+
+const isRecordSchema = (schema: Schema): schema is RecordSchema => schema.tag == RecordTag
+
+export const RecordEncoderPlugin: EncoderPlugin = [
+	{
+		tag: RecordTag,
+		encode(schema, callPlugin, value) {
+			assert(isRecordSchema(schema), HERE)
+
+			if (isRecord(value))
+				return callPlugin(schema.schema, getKeys(value).map(key => ({ key, value: value[key] })))
+		}
+	},
+	ArrayEncoderPlugin,
+	ObjectEncoderPlugin
+]
+
+export const RecordDecoderPlugin: DecoderPlugin = [
+	{
+		tag: RecordTag,
+		decode(schema, callPlugin) {
+			assert(isRecordSchema(schema), HERE)
+
+			return Object.fromEntries(callPlugin(schema.schema).map(({ key, value }) => [ key, value ]))
+		}
+	},
+	ArrayDecoderPlugin,
+	ObjectDecoderPlugin
+]
